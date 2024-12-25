23f88e0c344a85b50f0673b35a70a9606ace2855
Goutham Ganesan <26412305+gouthamganesan@users.noreply.github.com>
2024-12-25 07:15:03 +0530

    Implementing cuisine filters in list view

--- Views/RecipeList.swift
+++ Views/RecipeList.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct RecipeList: View {
+    @State private var selectedCuisine: String = "All"
+    
+    private var filteredRecipes: [Recipe] {
+        selectedCuisine == "All" ? recipes : recipes.filter { $0.cuisine == selectedCuisine }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List(recipes, id: \.id) { recipe in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(["All"] + Recipe.cuisineList, id: \.self) { cuisine in
+                        Button(action: {
+                            selectedCuisine = cuisine
+                        }) {
+                            Text(cuisine)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 5)
+                                .background(cuisine == selectedCuisine ? Color.blue : Color.gray.opacity(0.2))
+                                .foregroundColor(cuisine == selectedCuisine ? .white : .black)
+                                .clipShape(Capsule())
+                        }
+                    }
+                }
+                .padding(.horizontal, 8)
+            }
+            .padding([.leading, .trailing])
+            List(filteredRecipes, id: \.id) { recipe in
                 NavigationLink {
                     RecipeDetail(recipe: recipe)
                 } label: {
